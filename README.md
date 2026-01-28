f60cf4f67023e1bcd72657ce3eabc77856bdc3a9
mmanueljoe <emmanuelletsu18@gmail.com>
2026-01-28 22:25:44 +0000

    setup: scaffold project and setup linting, prettier

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kanban Task Manager
+
+## Description
+
+A simple kanban task manager built with React and Tailwind CSS focused on learning and practicing routing with React Router.
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      '@': new URL('/src', import.meta.url).pathname,
+      '@types': new URL('/src/types', import.meta.url).pathname,
+      '@assets': new URL('/src/assets', import.meta.url).pathname,
+      '@components': new URL('/src/components', import.meta.url).pathname,
+      '@utils': new URL('/src/utils', import.meta.url).pathname,
+      '@hooks': new URL('/src/hooks', import.meta.url).pathname,
+      '@contexts': new URL('/src/contexts', import.meta.url).pathname,
+    },
+  },
+})
